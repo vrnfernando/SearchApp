7d829c0a4846dfc8bb3d3af2ab5900b3189a7187
Vishwa Fernando <vishwafernando@Vishwas-MacBook-Pro.local>
2022-06-12 14:13:37 +0530

    Initial commit

---
+++ SearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchApp
+//
+//  Created by Vishwa Fernando on 6/12/22.
+//  Copyright © 2022 ncs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
